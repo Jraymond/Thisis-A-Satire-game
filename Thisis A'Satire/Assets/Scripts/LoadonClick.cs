cd86f65c70f0a2a991a9dbb3ec090baee319ea7f
Josh <Jraymond@student.dwc.edu>
2018-05-24 00:21:58 -0400

    first game files commit

---
+++ Thisis A'Satire/Assets/Scripts/LoadonClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadonClick : MonoBehaviour {
+
+	public void loadscene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
